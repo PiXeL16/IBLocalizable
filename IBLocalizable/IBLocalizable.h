21a6511cf7494697c166863d5a3cae33307fc741
Chris Jimenez <cjimenez16@gmail.com>
2016-07-29 21:04:17 -0600

    Initial Commit

---
+++ IBLocalizable/IBLocalizable.h
@@ -0,0 +1,19 @@
+//
+//  IBLocalizable.h
+//  IBLocalizable
+//
+//  Created by Chris Jimenez on 7/29/16.
+//  Copyright © 2016 Chris Jimenez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBLocalizable.
+FOUNDATION_EXPORT double IBLocalizableVersionNumber;
+
+//! Project version string for IBLocalizable.
+FOUNDATION_EXPORT const unsigned char IBLocalizableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBLocalizable/PublicHeader.h>
+
+
