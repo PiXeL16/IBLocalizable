f789bcd12878858230aa953080a83a321d775715
Chris Jimenez <cjimenez16@gmail.com>
2016-08-06 12:06:55 -0600

    Add UISearchBar support to the list of Localizable views :)

---
+++ IBLocalizable/Views/LocalizableUISearchBar.swift
@@ -0,0 +1,24 @@
+//
+//  LocalizableUISearchBar.swift
+//  IBLocalizable
+//
+//  Created by Chris Jimenez on 8/6/16.
+//  Copyright © 2016 Chris Jimenez. All rights reserved.
+//
+
+import UIKit
+// MARK: - Button extension that adds the localizable property
+extension UISearchBar {
+    
+    public override var localizableProperty: String?{
+        
+        get{
+            return self.placeholder
+        }
+        set{
+            self.placeholder = newValue
+        }
+    }
+    
+    
+}
